5c67ece04b92aa0b717c1952a48bf98526e40c3d
Md Irfan <irfanjunaid78081@gmail.com>
2023-02-19 20:36:13 +0530

    pagination and filteration completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,10 +2,32 @@ const axios = require("axios");
 const { User } = require("../model/User.model");
 
 const getAllUsers = async (req, res) => {
+  const { gender, city, state, country, page, limit } = req.query;
   let users;
+  const query = {};
+
+  let queryPage = Number(page) || 1;
+  let queryLimit = Number(limit) || 10;
+  let skip = (queryPage - 1) * queryLimit;
+
+  if (gender) {
+    query.gender = gender.toLowerCase();
+  }
+
+  if (city) {
+    query["location.city"] = city;
+  }
+
+  if (state) {
+    query["location.state"] = state;
+  }
+
+  if (country) {
+    query["location.country"] = country;
+  }
 
   try {
-    users = await User.find();
+    users = await User.find(query).skip(skip).limit(queryLimit);
   } catch (error) {
     return res.status(400).json(error.message);
   }
@@ -18,6 +40,18 @@ const getAllUsers = async (req, res) => {
 };
 
 const addUsers = async (req, res) => {
+  let users;
+
+  try {
+    users = await User.find();
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+
+  if (users.length > 0) {
+    return res.status(200).json({ message: "Data is already fetched" });
+  }
+
   const fetchedData = await axios.get("https://randomuser.me/api/?results=55");
 
   const data = fetchedData.data.results;
@@ -33,7 +67,7 @@ const addUsers = async (req, res) => {
     return res.status(404).json({ message: "Failed to add users" });
   }
 
-  return res.status(200).json(result);
+  return res.status(200).json({ message: "Users successfully fetched" });
 };
 
 const deleteUsers = async (req, res) => {
