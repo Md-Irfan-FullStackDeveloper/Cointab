13cd4bc0ec89da09164529133ed86c3842231ca9
Md Irfan <irfanjunaid78081@gmail.com>
2023-02-19 23:48:08 +0530

    project is done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,7 +29,7 @@ const getAllUsers = async (req, res) => {
 
   try {
     users = await User.find(query).skip(skip).limit(queryLimit);
-    let temp = await User.find()
+    let temp = await User.find(query)
     totalpage = Math.ceil(temp.length / 10);
   } catch (error) {
     return res.status(400).json(error.message);
